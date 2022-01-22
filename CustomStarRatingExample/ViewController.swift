689b5375b88437a0df67fce35fb4ba002faed5e0
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2022-01-22 15:42:46 +0300

    Initial Commit

---
+++ CustomStarRatingExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomStarRatingExample
+//
+//  Created by Олег Федоров on 22.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
